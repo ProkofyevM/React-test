e410af8d65181f98c2d831b79a54a4f3d691a517
Миша Прокофьев <misaprokofev@MacBook-Air--Misha.local>
2024-02-02 05:49:27 +0300

    createElement coding

--- src/App.js
+++ src/App.js
@@ -6,23 +6,31 @@ import React from 'react'
 //Все декларативный кроме span, он иперативный!
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<span>{new Date().getFullYear()}</span>
-			</header>
-		</div>
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit',
+				React.createElement('code', null, 'src / App.js'),
+				'and save to reload.',
+			),
+			React.createElement('a', {
+				className: 'App-link',
+				href: 'https://reactjs.org',
+				target: '_blank',
+				rel: 'noopener noreferrer',
+			}),
+			React.createElement('span', null, new Date().getFullYear()),
+		),
 	)
 }
